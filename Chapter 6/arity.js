2a7b8b0f0a671c93b6ea8736b27810948000a000
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2022-03-21 09:40:15 -0500

    making sure that the function returned by arity has the correct length property

--- Chapter 6/arity.js
+++ Chapter 6/arity.js
@@ -1,5 +1,9 @@
-const arity =
-  (fn, n) =>
-  (...args) =>
-    fn(...args.slice(0, n));
+const arity = (fn, n) =>
+  Object.defineProperty((...args) => fn(...args.slice(0, n)), "length", {
+    value: n,
+  });
+
 module.exports = arity;
+
+const fn = (x, y) => x + y;
+console.log(arity(fn, 2).length);
