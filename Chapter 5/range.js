667aec9a097a5b0a78dec1e1b9518f74276a8154
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2022-03-21 22:22:56 -0500

    commenting logs

--- Chapter 5/range.js
+++ Chapter 5/range.js
@@ -15,9 +15,9 @@ const range = (start = 0, end, step = 1) => {
     );
 };
 
-console.log(range(1, 10, 2));
-console.log(range(1, 5));
-console.log(range(0, 20, 5));
-console.log(range(0));
+// console.log(range(1, 10, 2));
+// console.log(range(1, 5));
+// console.log(range(0, 20, 5));
+// console.log(range(0));
 
 module.exports = range;
