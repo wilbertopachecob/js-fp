65d5083f341eeeed5cc0efeda941ba5f6035159c
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2022-03-26 00:09:20 -0500

    refactoring

--- Chapter_8/exercises/headline.js
+++ Chapter_8/exercises/headline.js
@@ -1,16 +1,18 @@
 const demethodize = require("../../Chapter_6/demethodize");
 const curry = require("../../Chapter_7/curryBind");
 const { flip2 } = require("../../Chapter_7/flip");
-const pipeline2 = require("../pipeline");
+const pipeline = require("../pipeline");
 
 const toLowerCase = demethodize(String.prototype.toLowerCase);
-let map = demethodize(Array.prototype.map);
-map = flip2(map);
-map = curry(map);
+const map = pipeline(demethodize, flip2, curry)(Array.prototype.map);
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 const splitWords = (s) => s.split(" ");
 const arrayToString = (arr) => arr.join(" ");
-const headline = (s) =>
-  pipeline2(toLowerCase, splitWords, map(capitalize), arrayToString)(s);
+const headline = pipeline(
+  toLowerCase,
+  splitWords,
+  map(capitalize),
+  arrayToString
+);
 
 console.log(headline("Alice's ADVENTURES in WoNdErLaNd"));
