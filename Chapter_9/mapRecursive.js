ce8c97ff0836acd57133917e110c12c647de3402
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2022-04-07 23:14:30 -0500

    fixing issue with empty value

--- Chapter_9/mapRecursive.js
+++ Chapter_9/mapRecursive.js
@@ -1,4 +1,42 @@
-const mapRecursive = (arr, fn) =>
-  arr.length === 0 ? [] : [fn(arr[0])].concat(arr.slice(1), fn);
+const mapRecursive = (arr, fn, i = 0, result = arr) =>
+  result.length === 0
+    ? []
+    : [fn(result[0], i, arr)].concat(
+        mapRecursive(arr, fn, ++i, result.slice(1))
+      );
 
-module.exports = mapRecursive;
+let aaa = [1, 2, 4, 5, 7];
+const timesTen = (x) => x * 10;
+// console.log(aaa.map(timesTen)); // [10, 20, 40, 50, 70]
+// console.log(mapRecursive(aaa, timesTen));
+
+//avoiding user to mess up with arguments
+const mapRecursive2 = (orig, cb) => {
+  const mapLoop = (arr, i) =>
+    arr.length === 0
+      ? []
+      : [cb(arr[0], i, orig)].concat(mapLoop(arr.slice(1), i + 1));
+
+  return mapLoop(orig, 0);
+};
+// console.log(mapRecursive2(aaa, timesTen));
+
+//respecting empty values
+const mapRecursive3 = (orig, cb) => {
+  const mapLoop = (arr, i) => {
+    if (arr.length === 0) {
+      return [];
+    }
+    if (!(0 in arr)) {
+      return [,].concat(mapLoop(arr.slice(1), i + 1));
+    }
+    return [cb(arr[0], i, orig)].concat(mapLoop(arr.slice(1), i + 1));
+  };
+  return mapLoop(orig, 0);
+};
+
+console.log(mapRecursive3(aaa, timesTen));
+console.log(mapRecursive3([1, 2, , , 5], timesTen));
+console.log([1, 2, , , 5].map(timesTen));
+
+module.exports = mapRecursive3;
