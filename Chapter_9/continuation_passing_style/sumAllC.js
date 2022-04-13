018455acd066b0779bd0b9d193dc57703086dcac
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2022-04-13 16:42:02 -0500

    working with continuation passing style

--- Chapter_9/continuation_passing_style/sumAllC.js
+++ Chapter_9/continuation_passing_style/sumAllC.js
@@ -5,13 +5,17 @@ const trampoline = (fn) => {
   return fn;
 };
 
-const sumAllC = (n, cont) =>
-  n === 0 ? cont(0) : sumAllC(n - 1, (v) => cont(v + n));
+const sumAllC = (n, cont) => {
+  // console.log(cont.toString());
+  return n === 0 ? cont(0) : sumAllC(n - 1, (v) => cont(v + n));
+};
 
 const sumAllT = (n, cont) =>
   n === 0 ? () => cont(0) : () => sumAllT(n - 1, (v) => () => cont(v + n));
 
 const sumAll2 = (n) => trampoline(sumAllT(n, (x) => x));
 
-// sumAllC(10000, console.log); // RangeError: Maximum call stack size exceeded
-console.log(sumAll2(10000));
+sumAllC(10, console.log); // RangeError: Maximum call stack size exceeded
+// console.log(sumAll2(10000));
+
+// (v) => ((v) => cont(v + n))(v + n)
